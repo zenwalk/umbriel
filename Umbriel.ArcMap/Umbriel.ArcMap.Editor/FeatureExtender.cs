bd64927257b8cffeda0cc2b638cc84cfc58d34b5
cumminsjp <cumminsjp@d79c235c-81b4-11de-8aaf-8d8425684b44>
2009-08-06 20:06:56 +0000

    Initial check-in of the editor / batch extend form

---
+++ Umbriel.ArcMap/Umbriel.ArcMap.Editor/FeatureExtender.cs
@@ -0,0 +1,367 @@
+﻿// <copyright file="BatchExtend.cs" company="Earth">
+// Copyright (c) 2009 All Rights Reserved
+// </copyright>
+// <author>Jay Cummins</author>
+// <email>cumminsjp@gmail.com</email>
+// <date>2009-08-06</date>
+// <summary>Feature Extender class file</summary>
+
+namespace Umbriel.ArcMap.Editor
+{
+    using System;
+    using System.Collections.Generic;
+    using ESRI.ArcGIS.ArcMapUI;
+    using ESRI.ArcGIS.Carto;
+    using ESRI.ArcGIS.Framework;
+    using ESRI.ArcGIS.Editor;
+    using ESRI.ArcGIS.Geometry;
+    using ESRI.ArcGIS.Geodatabase;
+
+    /// <summary>
+    /// enum from to line
+    /// </summary>
+    internal enum LineEndPoint
+    {
+        /// <summary>
+        /// from vertex of line
+        /// </summary>
+        From,
+
+        /// <summary>
+        /// to vertex of line
+        /// </summary>
+        To
+    }
+
+    /// <summary>
+    /// TargetItem structure for results from spatial query
+    /// </summary>
+    internal struct TargetItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        internal IFeature targetFeature;
+        internal LineEndPoint nearestEndPoint;
+
+        internal TargetItem(IFeature feature, LineEndPoint endPoint)
+        {
+            targetFeature = feature;
+            nearestEndPoint = endPoint;
+        }
+
+    }
+
+    public delegate void ExtendMessageEventHandler(string message);
+    public delegate void ExtendFeatureProgressHandler(int currentItemIndex, int totalItems, string message);
+    public delegate void AfterExtendFeaturesHandler(int featureAttempted, int featuresExtended, int featuresNotExtended);
+
+    public sealed class FeatureExtender
+    {
+        private List<IFeatureLayer> FeatureLayers { get; set; }
+        private IMxDocument MxDocument { get; set; }
+        private IApplication ArcMapApplication { get; set; }
+        private IEditor EditorExtension { get; set; }
+
+        private int CounterExtended { get; set; }
+        private int CounterNotExtended { get; set; }
+
+        public bool Simulation { get; set; }
+        public double SearchTolerance { get; set; }
+
+
+        #region Events
+        public event ExtendMessageEventHandler ExtendMessageEvent;
+        public event ExtendFeatureProgressHandler ExtendFeatureProgressEvent;
+        public event AfterExtendFeaturesHandler AfterExtendFeaturesEvent;
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeatureExtender"/> class.
+        /// </summary>
+        /// <param name="layers">The layers.</param>
+        /// <param name="arcmapApplication">The arcmap application.</param>
+        public FeatureExtender(List<IFeatureLayer> layers, IApplication arcmapApplication)
+        {
+            this.FeatureLayers = layers;
+            this.ArcMapApplication = arcmapApplication;
+            this.MxDocument = (IMxDocument)arcmapApplication.Document;
+            this.EditorExtension = (IEditor)this.ArcMapApplication.FindExtensionByName("ESRI Object Editor");
+            this.Simulation = true;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeatureExtender"/> class.
+        /// </summary>
+        /// <param name="layers">The layers.</param>
+        /// <param name="arcmapApplication">The arcmap application.</param>
+        /// <param name="searchTolerance">The search tolerance.</param>
+        public FeatureExtender(List<IFeatureLayer> layers, IApplication arcmapApplication, double searchTolerance)
+        {
+            this.SearchTolerance = searchTolerance;
+            this.FeatureLayers = layers;
+            this.ArcMapApplication = arcmapApplication;
+            this.MxDocument = (IMxDocument)arcmapApplication.Document;
+            this.EditorExtension = (IEditor)this.ArcMapApplication.FindExtensionByName("ESRI Object Editor");
+            this.Simulation = true;
+        }
+        #endregion
+
+
+        public void Extend(IFeatureSelection featureSelection)
+        {
+            Extend(featureSelection, this.SearchTolerance);
+        }
+
+        public void Extend(IFeatureSelection featureSelection, double searchTolerance)
+        {
+            IFeatureLayer featureLayer = (IFeatureLayer)featureSelection;
+            IEnumIDs enumIDs = featureSelection.SelectionSet.IDs;
+            int i = -1;
+            int c = 0;
+            int t = featureSelection.SelectionSet.Count;
+
+            this.CounterExtended = 0;
+            this.CounterNotExtended = 0;
+
+            while ((i = enumIDs.Next()) != -1)
+            {
+                try
+                {
+                    c++;
+                    OnExtendFeatureProgress(c, t, "Begin");
+                    IFeature feature = featureLayer.FeatureClass.GetFeature(i);
+                    Extend(feature, searchTolerance);
+                    OnExtendFeatureProgress(c, t, "End");
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Trace.WriteLine("Exception on " + c.ToString() + " of " + t.ToString());
+                    System.Diagnostics.Trace.WriteLine(ex.StackTrace);
+                }
+            }
+
+            OnAfterExtendFeatures(t, this.CounterExtended, this.CounterNotExtended);
+        }
+
+        /// <summary>
+        /// Extends the specified feature.
+        /// </summary>
+        /// <param name="feature">The feature.</param>
+        public void Extend(IFeature feature)
+        {
+            Extend(feature, this.SearchTolerance);
+        }
+
+        /// <summary>
+        /// Extends the specified feature.
+        /// </summary>
+        /// <param name="feature">The feature to extend</param>
+        /// <param name="searchTolerance">The search tolerance.</param>
+        public void Extend(IFeature feature, double searchTolerance)
+        {
+            try
+            {
+                IPolyline polyLine = (IPolyline)feature.Shape;
+
+                // TODO: drop a TargetItem from the list if it has been extended.
+                foreach (IFeatureLayer layer in this.FeatureLayers)
+                {
+                    List<TargetItem> targetFeatures = GetTargetFeatures(layer, searchTolerance, (IGeometry)polyLine);
+
+                    if (targetFeatures.Count.Equals(1))
+                    {
+                        TargetItem targetFeatureItem = targetFeatures[0];
+
+                        IConstructCurve constructCurve = new PolylineClass();
+
+                        esriSegmentExtension ext;
+
+                        if (targetFeatures[0].nearestEndPoint.Equals(LineEndPoint.From))
+                        {
+                            ext = esriSegmentExtension.esriExtendAtFrom;
+                        }
+                        else
+                        {
+                            ext = esriSegmentExtension.esriExtendAtTo;
+                        }
+
+                        bool extPerformed = false;
+
+                        constructCurve.ConstructExtended((ICurve)polyLine, (ICurve)targetFeatureItem.targetFeature.Shape, (int)esriCurveExtension.esriRelocateEnds, ref extPerformed);
+
+                        if (extPerformed)
+                        {
+                            IPolyline newLine = (IPolyline)constructCurve;
+
+                            System.Diagnostics.Debug.WriteLine("original length=  " + polyLine.Length.ToString());
+                            System.Diagnostics.Debug.WriteLine("newLine length=  " + newLine.Length.ToString());
+
+                            OnExtendMessage("Feature {OID} extended!!!", feature);
+
+                            if (!this.Simulation)
+                            {
+                                UpdateGeometry(feature, (IGeometry)newLine);
+                            }
+
+                            CounterExtended++;
+                        }
+                        else
+                        {
+                            OnExtendMessage("Feature {OID}  could not be extended.", feature);
+                            CounterNotExtended++;
+                        }
+                    }
+                    else if (targetFeatures.Count.Equals(0))
+                    {
+                        OnExtendMessage("Feature {OID}  is not within any features in: " + layer.Name, feature);
+                        CounterNotExtended++;
+                    }
+                    else
+                    {
+                        // TODO: handle multiple targets.
+                        OnExtendMessage("Feature {OID}  is within multiple features in: " + layer.Name + ".  Cannot extend this feature.", feature);
+                        CounterNotExtended++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.StackTrace);
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// Gets the target features.
+        /// </summary>
+        /// <param name="layer">The layer.</param>
+        /// <param name="searchTolerance">The search tolerance.</param>
+        /// <param name="featureGeom">The feature geom.</param>
+        /// <returns></returns>
+        private List<TargetItem> GetTargetFeatures(IFeatureLayer layer, double searchTolerance, IGeometry featureGeom)
+        {
+            IPolyline polyLine = (IPolyline)featureGeom;
+
+            ISpatialFilter filter = new SpatialFilterClass();
+            IGeometry geometry;
+            IFeatureCursor cursor;
+            IFeature cursorFeature;
+            ITopologicalOperator topoOperator;
+
+            List<TargetItem> targetFeatures = new List<TargetItem>();
+
+            //first, try the from point:
+            topoOperator = (ITopologicalOperator)polyLine.FromPoint;
+            geometry = topoOperator.Buffer(searchTolerance);
+            filter.Geometry = geometry;
+            filter.SpatialRel = esriSpatialRelEnum.esriSpatialRelIntersects;
+
+            cursor = layer.Search(filter, false);
+
+            while ((cursorFeature = cursor.NextFeature()) != null)
+            {
+                targetFeatures.Add(new TargetItem(cursorFeature, LineEndPoint.From));
+            }
+
+            ReleaseCOMObject(cursor);
+
+            //if the count is empty, try it with the other end:
+            if (targetFeatures.Count.Equals(0))
+            {
+                topoOperator = (ITopologicalOperator)polyLine.ToPoint;
+                geometry = topoOperator.Buffer(searchTolerance);
+                filter.Geometry = geometry;
+                filter.SpatialRel = esriSpatialRelEnum.esriSpatialRelIntersects;
+
+                cursor = layer.Search(filter, false);
+
+                while ((cursorFeature = cursor.NextFeature()) != null)
+                {
+                    targetFeatures.Add(new TargetItem(cursorFeature, LineEndPoint.To));
+                }
+
+                ReleaseCOMObject(cursor);
+            }
+
+            return targetFeatures;
+        }
+
+        /// <summary>
+        /// Called when [extend message].
+        /// </summary>
+        /// <param name="message">The extend status message</param>
+        private void OnExtendMessage(string message, IFeature feature)
+        {
+            if (this.ExtendMessageEvent != null)
+            {
+                string oidstring = string.Empty;
+                if (feature != null && feature.HasOID)
+                {
+                    oidstring = "(OID=" + feature.OID.ToString() + ")";
+                }
+
+                message = message.Replace("{OID}", oidstring);
+
+                this.ExtendMessageEvent(message);
+            }
+        }
+
+        /// <summary>
+        /// Called when [extend feature progress].
+        /// </summary>
+        /// <param name="i">The current index of the feature being extended.</param>
+        /// <param name="t">The total number of features being extended.</param>
+        /// <param name="message">The message.</param>
+        private void OnExtendFeatureProgress(int i, int t, string message)
+        {
+            if (this.ExtendFeatureProgressEvent != null)
+            {
+                this.ExtendFeatureProgressEvent(i, t, message);
+            }
+        }
+
+        /// <summary>
+        /// Called when [after extend features handler].
+        /// </summary>
+        /// <param name="total">The total.</param>
+        /// <param name="extended">The extended.</param>
+        /// <param name="failed">The failed.</param>
+        private void OnAfterExtendFeatures(int total, int extended, int failed)
+        {
+            if (this.AfterExtendFeaturesEvent != null)
+            {
+                this.AfterExtendFeaturesEvent(total, extended, failed);
+            }
+        }
+        private void UpdateGeometry(IFeature feature, IGeometry newGeometry)
+        {
+            if (this.EditorExtension.EditState == esriEditState.esriStateEditing)
+            {
+                this.EditorExtension.StartOperation();
+                feature.Shape = newGeometry;
+                feature.Store();
+                this.EditorExtension.StopOperation("Extend Feature");
+            }
+            else
+            {
+                throw new Exception("Unable to update geometry.  You must be editing in order to update geometry.");
+            }
+
+        }
+
+        private void ReleaseCOMObject(object o)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(o);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ Umbriel.ArcMap/Umbriel.ArcMap.Editor/UI/BatchExtendForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Umbriel.ArcMap.Editor.UI
+{
+    partial class BatchExtendForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BatchExtendForm));
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.labelExtendSelected = new System.Windows.Forms.Label();
+            this.checkedList = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSearchTolerance = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkSimulation = new System.Windows.Forms.CheckBox();
+            this.comboBoxFeatureLayers = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Enabled = false;
+            this.buttonOK.Location = new System.Drawing.Point(175, 252);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 34);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "&Extend";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(256, 252);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 34);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // labelExtendSelected
+            // 
+            this.labelExtendSelected.AutoSize = true;
+            this.labelExtendSelected.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelExtendSelected.Location = new System.Drawing.Point(7, 14);
+            this.labelExtendSelected.Name = "labelExtendSelected";
+            this.labelExtendSelected.Size = new System.Drawing.Size(196, 19);
+            this.labelExtendSelected.TabIndex = 1;
+            this.labelExtendSelected.Text = "Extend Selected Features in: ";
+            // 
+            // checkedList
+            // 
+            this.checkedList.CheckOnClick = true;
+            this.checkedList.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedList.FormattingEnabled = true;
+            this.checkedList.Location = new System.Drawing.Point(206, 80);
+            this.checkedList.Name = "checkedList";
+            this.checkedList.Size = new System.Drawing.Size(294, 70);
+            this.checkedList.TabIndex = 2;
+            this.checkedList.SelectedIndexChanged += new System.EventHandler(this.checkedList_SelectedIndexChanged);
+            this.checkedList.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedList_ItemCheck);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "... that are within ";
+            // 
+            // textBoxSearchTolerance
+            // 
+            this.textBoxSearchTolerance.Location = new System.Drawing.Point(206, 175);
+            this.textBoxSearchTolerance.Name = "textBoxSearchTolerance";
+            this.textBoxSearchTolerance.Size = new System.Drawing.Size(48, 23);
+            this.textBoxSearchTolerance.TabIndex = 4;
+            this.textBoxSearchTolerance.Text = "1";
+            this.textBoxSearchTolerance.TextChanged += new System.EventHandler(this.textBoxSearchTolerance_TextChanged);
+            this.textBoxSearchTolerance.Validated += new System.EventHandler(this.textBoxSearchTolerance_Validated);
+            this.textBoxSearchTolerance.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxSearchTolerance_Validating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(261, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "map unit(s).";
+            // 
+            // checkSimulation
+            // 
+            this.checkSimulation.AutoSize = true;
+            this.checkSimulation.Location = new System.Drawing.Point(175, 227);
+            this.checkSimulation.Name = "checkSimulation";
+            this.checkSimulation.Size = new System.Drawing.Size(120, 19);
+            this.checkSimulation.TabIndex = 6;
+            this.checkSimulation.Text = "Simulation Mode";
+            this.checkSimulation.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxFeatureLayers
+            // 
+            this.comboBoxFeatureLayers.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxFeatureLayers.FormattingEnabled = true;
+            this.comboBoxFeatureLayers.Location = new System.Drawing.Point(206, 12);
+            this.comboBoxFeatureLayers.Name = "comboBoxFeatureLayers";
+            this.comboBoxFeatureLayers.Size = new System.Drawing.Size(278, 23);
+            this.comboBoxFeatureLayers.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "...to the closest feature in:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel,
+            this.toolStripProgressBar});
+            this.statusStrip.Location = new System.Drawing.Point(0, 290);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(506, 22);
+            this.statusStrip.TabIndex = 9;
+            this.statusStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip_ItemClicked);
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.AutoSize = false;
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(180, 17);
+            this.toolStripStatusLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.toolStripStatusLabel.Click += new System.EventHandler(this.toolStripStatusLabel_Click);
+            // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Size = new System.Drawing.Size(200, 16);
+            this.toolStripProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.toolStripProgressBar.Visible = false;
+            // 
+            // BatchExtendForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 312);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxFeatureLayers);
+            this.Controls.Add(this.checkSimulation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSearchTolerance);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedList);
+            this.Controls.Add(this.labelExtendSelected);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonOK);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BatchExtendForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Batch Extend";
+            this.Load += new System.EventHandler(this.BatchExtendForm_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelExtendSelected;
+        private System.Windows.Forms.CheckedListBox checkedList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSearchTolerance;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkSimulation;
+        private System.Windows.Forms.ComboBox comboBoxFeatureLayers;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+    }
+}
