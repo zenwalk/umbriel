9231484244adc5448626d13b56a5dea0b7ccba88
cumminsjp <cumminsjp@d79c235c-81b4-11de-8aaf-8d8425684b44>
2009-09-17 19:01:10 +0000

    added geohasher class.

---
+++ Umbriel.ArcMap/Umbriel.ArcMap.Editor/Util/Geohasher.cs
@@ -0,0 +1,91 @@
+﻿// <copyright file="Geohasher.cs" company="Earth">
+// Copyright (c) 2009 All Rights Reserved
+// </copyright>
+// <author>Jay Cummins</author>
+// <email>cumminsjp@gmail.com</email>
+// <date>2009-08-25</date>
+// <summary>Geohasher class file</summary>
+
+namespace Umbriel.ArcMap.Editor.Util
+{
+    using ESRI.ArcGIS.Geometry;
+
+    /// <summary>
+    /// Creates a geohash from an ESRI IPoint
+    /// </summary>
+    public class Geohasher
+    {
+        /// <summary>
+        /// default precision when none is specified
+        /// </summary>
+        private const int DefaultPrecision = 13;
+
+
+        /// <summary>
+        /// Creates the geohash for IPoint
+        /// </summary>
+        /// <param name="pt">IPoint (must contain spatial reference)</param>
+        /// <param name="precision">The precision.</param>
+        /// <returns>geohash string</returns>
+        public static string CreateGeohash(IPoint pt,  int precision)
+        {
+            ISpatialReference spatialReference = WGS84SpatialReference();
+            pt.Project(spatialReference);
+            string geohash = Umbriel.GIS.Geohash.EncodeCoordinate(pt.Y, pt.X, precision);
+
+            return geohash;
+        }
+
+        /// <summary>
+        /// Creates the geohash for IPoint
+        /// </summary>
+        /// <param name="pt">IPoint (must contain spatial reference)</param>
+        /// <param name="spatialReference">The spatial reference to project to</param>
+        /// <param name="precision">The precision.</param>
+        /// <returns>geohash string</returns>
+        public static string CreateGeohash(IPoint pt, ISpatialReference spatialReference, int precision)
+        {
+            pt.Project(spatialReference);
+            string geohash = Umbriel.GIS.Geohash.EncodeCoordinate(pt.Y, pt.X, precision);
+
+            return geohash;            
+        }
+
+        /// <summary>
+        /// Creates the geohash for IPoint
+        /// </summary>
+        /// <param name="pt">IPoint (must contain spatial reference)</param>
+        /// <param name="spatialReference">The spatial reference to project to</param>
+        /// <returns>geohash string</returns>
+        public static string CreateGeohash(IPoint pt, ISpatialReference spatialReference)
+        {
+            return CreateGeohash(pt, spatialReference, DefaultPrecision);
+        }
+
+        /// <summary>
+        /// Creates the geohash for IPoint
+        /// </summary>
+        /// <param name="pt">IPoint (must contain spatial reference)</param>
+        /// <returns>geohash string</returns>
+        public static string CreateGeohash(IPoint pt)
+        {
+            ISpatialReference spatialReference = WGS84SpatialReference();
+            return CreateGeohash(pt, spatialReference, DefaultPrecision);
+        }
+
+        /// <summary>
+        /// Creates a WGS84 Spatial Reference
+        /// </summary>
+        /// <returns>ISpatialReference WGS84</returns>
+        public static ISpatialReference WGS84SpatialReference()
+        {
+            SpatialReferenceEnvironment spatialReferenceEnv = new SpatialReferenceEnvironmentClass();
+
+            IGeographicCoordinateSystem geoCS = spatialReferenceEnv.CreateGeographicCoordinateSystem((int)esriSRGeoCSType.esriSRGeoCS_WGS1984);
+            ISpatialReference spatialReference = geoCS;
+            spatialReference.SetFalseOriginAndUnits(-180, -90, 1000000);
+
+            return spatialReference;
+        }
+    }
+}
