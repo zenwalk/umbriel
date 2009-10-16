47a46b56661294f25533abd007eb8dadd6662cdd
cumminsjp <cumminsjp@d79c235c-81b4-11de-8aaf-8d8425684b44>
2009-10-16 20:34:18 +0000

    Added geohash calculator tool.  Little to no testing...works on versioned featureclasses.  Need to test none versioned/no edit session. No exception handling/commenting on this checkin

---
+++ Umbriel.ArcMapUI/UI/GeohashCalculatorForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Umbriel.ArcMapUI.UI
+{
+    partial class GeohashCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GeohashCalculatorForm));
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelLayerName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxFields = new System.Windows.Forms.ComboBox();
+            this.checkBoxUseSelected = new System.Windows.Forms.CheckBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalculate.Location = new System.Drawing.Point(89, 137);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(80, 32);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "C&alculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.Location = new System.Drawing.Point(175, 137);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(80, 32);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Layer:";
+            // 
+            // labelLayerName
+            // 
+            this.labelLayerName.AutoSize = true;
+            this.labelLayerName.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLayerName.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelLayerName.Location = new System.Drawing.Point(61, 9);
+            this.labelLayerName.Name = "labelLayerName";
+            this.labelLayerName.Size = new System.Drawing.Size(83, 18);
+            this.labelLayerName.TabIndex = 3;
+            this.labelLayerName.Text = "LayerName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select Geohash Field:";
+            // 
+            // comboBoxFields
+            // 
+            this.comboBoxFields.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxFields.FormattingEnabled = true;
+            this.comboBoxFields.Location = new System.Drawing.Point(12, 75);
+            this.comboBoxFields.Name = "comboBoxFields";
+            this.comboBoxFields.Size = new System.Drawing.Size(313, 27);
+            this.comboBoxFields.TabIndex = 5;
+            // 
+            // checkBoxUseSelected
+            // 
+            this.checkBoxUseSelected.AutoSize = true;
+            this.checkBoxUseSelected.Checked = true;
+            this.checkBoxUseSelected.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxUseSelected.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxUseSelected.Location = new System.Drawing.Point(25, 108);
+            this.checkBoxUseSelected.Name = "checkBoxUseSelected";
+            this.checkBoxUseSelected.Size = new System.Drawing.Size(256, 23);
+            this.checkBoxUseSelected.TabIndex = 6;
+            this.checkBoxUseSelected.Text = "Only Calculate Selected Features";
+            this.checkBoxUseSelected.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 181);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(337, 22);
+            this.statusStrip.TabIndex = 7;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(322, 17);
+            this.toolStripStatusLabel.Spring = true;
+            // 
+            // GeohashCalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 203);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.checkBoxUseSelected);
+            this.Controls.Add(this.comboBoxFields);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelLayerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonCalculate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GeohashCalculatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Geohash Calculator";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelLayerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxFields;
+        private System.Windows.Forms.CheckBox checkBoxUseSelected;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+    }
+}
---
+++ Umbriel.ArcMapUI/UI/GeohashCalculatorForm.cs
@@ -0,0 +1,231 @@
+﻿
+
+namespace Umbriel.ArcMapUI.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+    using System.Windows.Forms;
+    using ESRI.ArcGIS.ArcMapUI;
+    using ESRI.ArcGIS.Carto;
+    using ESRI.ArcGIS.Framework;
+    using ESRI.ArcGIS.esriSystem;
+    using ESRI.ArcGIS.Geodatabase;
+    using ESRI.ArcGIS.DataSourcesGDB;
+    using ESRI.ArcGIS.Geometry;
+    using ESRI.ArcGIS.Editor;
+
+    public partial class GeohashCalculatorForm : Form
+    {
+        private IApplication ArcMapApplication { get; set; }
+        private IFeatureLayer FeatureLayer { get; set; }
+        public IMxDocument MxDocument { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeohashCalculatorForm"/> class.
+        /// </summary>
+        public GeohashCalculatorForm()
+        {
+            labelLayerName.Text = string.Empty;
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeohashCalculatorForm"/> class.
+        /// </summary>
+        /// <param name="arcmapApp">The arcmap app.</param>
+        public GeohashCalculatorForm(IApplication arcmapApp)
+        {
+
+
+            InitializeComponent();
+
+            this.ArcMapApplication = arcmapApp;
+            this.MxDocument = (IMxDocument)arcmapApp.Document;
+
+            labelLayerName.Text = string.Empty;
+
+            LoadFeatureLayer();
+
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            IFeatureClass featureClass = this.FeatureLayer.FeatureClass;
+            IDataset dataset = (IDataset)featureClass;
+            IDatasetEdit datasetEdit = (IDatasetEdit)dataset;
+            
+            IVersionedObject versionedObject = (IVersionedObject)dataset;
+            IEditor editor = (IEditor)this.ArcMapApplication.FindExtensionByName("ESRI Object Editor");
+
+            bool versionedWorkspace = versionedObject.IsRegisteredAsVersioned;
+
+            if (versionedObject.IsRegisteredAsVersioned
+                && editor.EditState.Equals(esriEditState.esriStateNotEditing)
+                )
+            {
+                MessageBox.Show("You must be in an edit session when editing versioned feature classes.", "Geohash Calculator", MessageBoxButtons.OK);
+            }
+            else if (versionedObject.IsRegisteredAsVersioned
+                && editor.EditState.Equals(esriEditState.esriStateEditing)
+                && !datasetEdit.IsBeingEdited()
+                )
+            {
+                MessageBox.Show("You must be editing the same workspace as the highlighted layer.", "Geohash Calculator", MessageBoxButtons.OK);
+            }
+            else
+            {
+                Stack<int> objectIDs = GetObjectIds();
+
+                if (objectIDs.Count > 0)
+                {
+                    int fieldIndex = featureClass.FindField(comboBoxFields.Text);
+
+                    if (fieldIndex > -1)
+                    {
+                        int c = 0;
+
+                        foreach (int objectID in objectIDs)
+                        {
+                            c++;
+                            toolStripStatusLabel.Text = "Calculating   " + c.ToString() + " of " + objectIDs.Count.ToString() + " features.";
+                            statusStrip.Refresh();
+
+                            IFeature feature = featureClass.GetFeature(objectID);
+                            IGeometry geometry = (IGeometry)feature.Shape;
+
+                            string currentGeohashValue = "<Null>";
+
+                            if (!System.DBNull.Value.Equals(feature.get_Value(fieldIndex)))
+                            {
+                                currentGeohashValue = feature.get_Value(fieldIndex).ToString();
+                            }
+
+                            if (geometry is IPoint)
+                            {
+                                IPoint point = (IPoint)geometry;
+                                string geohash = Umbriel.ArcMap.Editor.Util.Geohasher.CreateGeohash(point, 13);
+
+                                if (!currentGeohashValue.Equals(geohash))
+                                {
+                                    if (versionedWorkspace)
+                                    {
+                                        editor.StartOperation();
+                                    }
+
+                                    feature.set_Value(fieldIndex, geohash);
+                                    feature.Store();
+
+                                    if (versionedWorkspace)
+                                    {
+                                        editor.StopOperation("Update Geohash Field.");
+                                    }
+                                }
+                                else
+                                {
+                                    toolStripStatusLabel.Text = "-- Same geohash. No Update Required --";
+                                    this.statusStrip.Refresh();
+                                }
+                            }
+                        }
+
+                        toolStripStatusLabel.Text = "-- Calculate Finished --";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("There are no features to calculate.", "Geohash Calculator", MessageBoxButtons.OK);
+                }
+            }   
+        }
+
+        private Stack<int> GetObjectIds()
+        {
+            Stack<int> objectIDs = new Stack<int>();
+            IFeatureSelection featureSelection = (IFeatureSelection)this.FeatureLayer;
+
+            if (checkBoxUseSelected.Checked && featureSelection.SelectionSet.Count > 0)
+            {
+                ISelectionSet selectionSet = featureSelection.SelectionSet;
+
+                IEnumIDs enumObjectIDs = selectionSet.IDs;
+
+                int i = -1;
+                while ((i = enumObjectIDs.Next()) != -1)
+                {
+                    objectIDs.Push(i);
+                }
+            }
+            else
+            {
+                IFeatureCursor cursor = this.FeatureLayer.FeatureClass.Search(null, true);
+                IFeature feature = null;
+
+                while ((feature = cursor.NextFeature()) != null)
+                {
+                    objectIDs.Push(feature.OID);
+                }
+            }
+
+            return objectIDs;
+        }
+
+        /// <summary>
+        /// Read in the selected feature layer and read in the fields
+        /// </summary>
+        private void LoadFeatureLayer()
+        {
+            if (this.MxDocument.SelectedLayer is IFeatureLayer)
+            {
+                this.FeatureLayer = (IFeatureLayer)this.MxDocument.SelectedLayer;
+                labelLayerName.Text = this.FeatureLayer.Name;
+                ReadAttributes();
+            }
+        }
+
+
+        /// <summary>
+        /// Reads the attributes from the feature layer
+        /// </summary>
+        private void ReadAttributes()
+        {
+            int geohashFieldIndex = -1;
+
+            if (this.FeatureLayer != null)
+            {
+                IFields fields = this.FeatureLayer.FeatureClass.Fields;
+
+                for (int i = 0; i < fields.FieldCount; i++)
+                {
+                    IField field = fields.get_Field(i);
+
+                    if (field.Type.Equals(esriFieldType.esriFieldTypeString))
+                    {
+                        comboBoxFields.Items.Add(field.Name);
+
+                        if (field.Name.IndexOf("geohash", StringComparison.CurrentCultureIgnoreCase) >= 0)
+                        {
+                            geohashFieldIndex = comboBoxFields.Items.Count - 1;
+                        }
+                    }
+                }
+            }
+
+            if (geohashFieldIndex >= 0)
+            {
+                comboBoxFields.SelectedIndex = geohashFieldIndex;
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
