3e5542eb2331194ef69465461effbd53e7493185
cumminsjp <cumminsjp@d79c235c-81b4-11de-8aaf-8d8425684b44>
2009-11-13 19:16:47 +0000

    initial checkin of the CreateLMStatTable.sql create SQL

---
+++ Umbriel.GIS/LMStat/CreateLMStatTable.sql
@@ -0,0 +1,24 @@
+﻿USE [working]
+GO
+
+/****** Object:  Table [dbo].[LMStat]    Script Date: 11/13/2009 13:20:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[LMStat](
+	[MachineName] [nvarchar](50) NOT NULL,
+	[LicenseName] [nvarchar](30) NOT NULL,
+	[TotalLicense] [int] NOT NULL,
+	[InUseLicense] [int] NOT NULL,
+	[StatusDate] [datetime] NOT NULL,
+	[DateCreated] [datetime] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[LMStat] ADD  CONSTRAINT [DF_LMStats_DateCreated]  DEFAULT (getdate()) FOR [DateCreated]
+GO
+
