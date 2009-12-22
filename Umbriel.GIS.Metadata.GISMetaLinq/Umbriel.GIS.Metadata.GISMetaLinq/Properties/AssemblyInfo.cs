a0f00a164c3b76cf9577c13f9f0f9053b79b5d23
cumminsjp <cumminsjp@d79c235c-81b4-11de-8aaf-8d8425684b44>
2009-12-22 16:38:47 +0000

    initial checkin

---
+++ Umbriel.GIS.Metadata.GISMetaLinq/Umbriel.GIS.Metadata.GISMetaLinq/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Umbriel.GIS.Metadata.GISMetaLinq")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jay Cummins")]
+[assembly: AssemblyProduct("Umbriel.GIS.Metadata.GISMetaLinq")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e00b6ea2-34cf-48ac-9d53-765fa9944919")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
