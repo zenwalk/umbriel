346772b97f0a8f144d2c76f805fbf5322db61803
cumminsjp <cumminsjp@d79c235c-81b4-11de-8aaf-8d8425684b44>
2009-12-22 16:38:47 +0000

    initial checkin

---
+++ trunk/Umbriel.GIS.Metadata.GISMetaLinq/Umbriel.GIS.Metadata.GISMetaLinq/Program.cs
@@ -0,0 +1,210 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Umbriel.GIS.Metadata.GISMetaLinq
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+
+        [STAThread()]
+        static void Main(string[] args)
+        {
+            // display the usage when
+            if (System.Environment.CommandLine.IndexOf("-h", 0, System.StringComparison.CurrentCultureIgnoreCase) >= 0 |
+                System.Environment.CommandLine.IndexOf("--help", 0, System.StringComparison.CurrentCultureIgnoreCase) >= 0 |
+                System.Environment.CommandLine.IndexOf("/?", 0, System.StringComparison.CurrentCultureIgnoreCase) >= 0 |
+                System.Environment.CommandLine.IndexOf("-help", 0, System.StringComparison.CurrentCultureIgnoreCase) >= 0)
+            {
+                Usage();
+                return;
+            }
+
+            if (ValidateArgs())
+            {
+                string infile = GetSwitchValue("-f");
+                string outfile = GetSwitchValue("-out");
+
+                string operation = GetSwitchValue("-o");
+
+                switch (operation)
+                {
+                    case "purgeanalyzesteps":
+                        PurgeAnalyzeStepsFromMetadata(infile, outfile);
+                        break;
+                    default:
+                        Trace.WriteLine("No case for: " + operation);
+                        break;
+                }
+
+
+            }
+        }
+
+        /// <summary>
+        /// Purges the analyze steps from metadata.
+        /// </summary>
+        /// <param name="infile">The infile path</param>
+        /// <param name="outfile">The outfile path</param>
+        private static void PurgeAnalyzeStepsFromMetadata(string infile, string outfile)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            XDocument doc = XDocument.Load(infile);
+            XElement root = doc.Root;
+
+            var d =
+                (from el in root.Descendants("Process")
+                 select el);
+
+            List<XElement> deletenodes = new List<XElement>();
+
+            
+            foreach (XElement node in d)
+            {
+                foreach (XAttribute attribute in node.Attributes())
+                {
+                    if (attribute.Value.ToString().StartsWith("Analyze"))
+                    {
+                        deletenodes.Add(node);
+                    }
+                }
+            }
+
+            int removecount = 0;
+
+            foreach (XElement node in deletenodes)
+            {
+                try
+                {
+                    // remove the node:
+                    node.Remove();
+                    Console.WriteLine("Delete Node: " + node.ToString());
+                    Console.WriteLine();
+                    removecount++;
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine("Delete Unsuccessful: " + ex.Message);
+                }                
+            }
+
+            doc.Save(outfile);
+
+            stopwatch.Stop();
+
+            Console.WriteLine("Delete Attempts: " + deletenodes.Count.ToString() + " Actual Deletes: " + removecount.ToString());
+            Console.WriteLine("Purge Analyze Steps Duration: " + (stopwatch.ElapsedTicks/1000).ToString("0.00000"));
+
+        }
+
+        /// <summary>
+        /// Validates the args.
+        /// </summary>
+        /// <returns></returns>
+        private static bool ValidateArgs()
+        {
+            bool valid = true;
+
+            //Dictionary<int, string> arguments = new Dictionary<int, string>(args);
+
+            List<string> arguments = new List<string>(System.Environment.GetCommandLineArgs());
+
+            try
+            {
+                if (!arguments.Contains("-f")
+                    | !arguments.Contains("-out"))
+                {
+                    valid = false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return valid;
+            //// get the esri product code: 
+            //try
+            //{
+            //    int productCode = 0;
+
+            //    if (ArgParser.Contains("-a"))
+            //    {
+            //        string licenseProductCode = ArgParser.GetValue("-a");
+            //        productCode = Convert.ToInt32(licenseProductCode);
+            //        ESRILicenseProductCode = (esriLicenseProductCode)productCode;
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    valid = false;
+            //}
+
+            // get the operation code:
+        }
+
+        private static string GetSwitchValue(string sw)
+        {
+            try
+            {
+                List<string> arguments = new List<string>(System.Environment.GetCommandLineArgs());
+
+                int i = arguments.IndexOf(sw);
+
+                string value = arguments[i + 1];
+
+                return value;
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Writes the usage to the console
+        /// </summary>
+        /// <returns>string containing the usage text to be displayed</returns>
+        private static string Usage()
+        {
+            try
+            {
+                System.Reflection.Assembly assemblyID = System.Reflection.Assembly.GetExecutingAssembly();
+                System.IO.Stream streamID = assemblyID.GetManifestResourceStream("GISMetaLinq.usg.txt");
+                System.IO.StreamReader sr = new System.IO.StreamReader(streamID);
+
+                string usage = sr.ReadToEnd();
+                Console.WriteLine(usage);
+
+                // tack on the version info:
+                string tail;
+                Version v = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                string ver = v.Major.ToString() + "." + v.Minor.ToString() + "." + v.Revision.ToString() + "." + v.Build.ToString();
+
+                tail = "sdemeta.exe " + ver;
+
+                Console.WriteLine(tail);
+
+                return usage;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error displaying usage text: ");
+                Console.WriteLine(ex.StackTrace);
+                Trace.WriteLine(ex.StackTrace);
+                return string.Empty;
+            }
+        }
+    }
+}
