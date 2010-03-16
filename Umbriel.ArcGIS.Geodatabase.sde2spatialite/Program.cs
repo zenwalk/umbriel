558fab12dc1459be91162fc9784d3d7e55389566
cumminsjp <cumminsjp@d79c235c-81b4-11de-8aaf-8d8425684b44>
2010-03-16 12:04:08 +0000

    Initial check-in of sde2spatialite

---
+++ Umbriel.ArcGIS.Geodatabase.sde2spatialite/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Umbriel.ArcGIS.Geodatabase.sde2spatialite
+{
+    class Program
+    {
+        [STAThread()]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
